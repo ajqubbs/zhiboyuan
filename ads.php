54514b6fafacd9cd7d3efe4df44b1a5b9628f987
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-03-04 23:39:14 +0800

    Update ads.php via web editor

--- ads.php
+++ ads.php
@@ -1,1 +1,1 @@
-{"id":"1","wenads":"公众号江南软件库 ","imads1":"https://free-img.400040.xyz/4/2025/07/13/68735b8b842c5.png","imads2":"http://xmsc.xcwluo.com/upload/images/20241229/17354447776770c929d3967.jpg","imads3":"顺为凡，逆则仙，只在心中一念间！","imads4":"","imads5":"","viads":"问鼎市场"}
+{"id":"1","wenads":"公众号江南软件库 ","imads1":"https://free-img.400040.xyz/4/2025/07/13/68735b8b842c5.png","imads2":"","imads3":"顺为凡，逆则仙，只在心中一念间！","imads4":"","imads5":"","viads":"问鼎市场"}
