e7e61843a48c6dc10fe97d1b25cd7a8f41191656
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-03-04 23:20:51 +0800

    Update index.php via web editor

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 {"id":"1","title":"叶子TV[腰疼]","logo":"https://free-img.400040.xyz/4/2025/07/13/687357b53b0ae.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iD2cK314rswb","image1":"https://free-img.400040.xyz/4/2025/07/14/6874a40c25533.jpg","image2":"https://free-img.400040.xyz/4/2025/07/14/6874a4106b5f0.jpg","daxiao":"36.6MB","create_time":"1 时以前 "}
 
-{"id":"2","title":"新台奥直播[港澳台]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a1dac6e5.png,"desc1":"影视必备","desc":"（看简介图片操作免登陆使用）","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iWhCz36jidrc","image1":"https://www.haitangw.cc/wp-content/uploads/2025/06/640-38.png","image2":"https://www.haitangw.cc/wp-content/uploads/2025/06/640-39.png","daxiao":"15.2MB","create_time":"1 时以前 "}  
+{"id":"2","title":"新台奥直播[港台]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a1dac6e5.png,"desc1":"影视必备","desc":"（看简介图片操作免登陆使用）","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iWhCz36jidrc","image1":"https://www.haitangw.cc/wp-content/uploads/2025/06/640-38.png","image2":"https://www.haitangw.cc/wp-content/uploads/2025/06/640-39.png","daxiao":"15.2MB","create_time":"1 时以前 "}  
 
-{"id":"3","title":"i优势在我","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a8349fa3.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/id6yG3jm65xe","image1":"https://pic.qtsyw.com/up/2025-6/17497838347537245.jpg","image2":"https://pic.wk2.com/up/2025-6/202561317933764860.png!400_400","daxiao":"18.2MB","create_time":"1 时以前 "} 
+{"id":"3","title":"i优势在我[港台]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a8349fa3.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/id6yG3jm65xe","image1":"https://pic.qtsyw.com/up/2025-6/17497838347537245.jpg","image2":"https://pic.wk2.com/up/2025-6/202561317933764860.png!400_400","daxiao":"18.2MB","create_time":"1 时以前 "} 
 
-{"id":"4","title":"i台奥直播","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a856267a.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iJrLp3jm67fi","image1":"","image2":"","daxiao":"18.8MB","create_time":"1 时以前 "}
+{"id":"4","title":"i台奥直播[港奥台]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a856267a.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iJrLp3jm67fi","image1":"","image2":"","daxiao":"18.8MB","create_time":"1 时以前 "}
 
-{"id":"5","title":"i千问直播","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a871f2f5.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iJrLp3jm67fi","image1":"","image2":"","daxiao":"18.8MB","create_time":"1 时以前 "}
+{"id":"5","title":"i千问直播[腰疼]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a871f2f5.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iJrLp3jm67fi","image1":"","image2":"","daxiao":"18.8MB","create_time":"1 时以前 "}
 
 
-{"id":"6","title":"新凡人直播","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a840b8a1.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iD9Yj3jj4ekf","image1":"","image2":"","daxiao":"18.8MB","create_time":"1 时以前 "}
+{"id":"6","title":"新凡人直播[腰疼]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a840b8a1.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iD9Yj3jj4ekf","image1":"","image2":"","daxiao":"18.8MB","create_time":"1 时以前 "}
 
 
 {"id":"15","title":"至尊4K","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a225da58.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iOo3t3jrnp1e","image1":"","image2":"","daxiao":"18.8MB","create_time":"1 时以前 "}
