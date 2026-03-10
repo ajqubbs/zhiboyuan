e9973c938372a0670a9b8fa54b57d50f1c1fc6f3
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-03-10 13:54:33 +0800

    Update index.php via web editor

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 {"id":"2","title":"叶子TV","logo":"https://free-img.400040.xyz/4/2025/07/13/687357b53b0ae.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iD2cK314rswb","image1":"https://free-img.400040.xyz/4/2025/07/14/6874a40c25533.jpg","image2":"https://free-img.400040.xyz/4/2025/07/14/6874a4106b5f0.jpg","daxiao":"36MB","create_time":"260304 "}
 
 
-
 {"id":"3","title":"i优势在我[港台]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a8349fa3.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/id6yG3jm65xe","image1":"https://pic.qtsyw.com/up/2025-6/17497838347537245.jpg","image2":"https://pic.wk2.com/up/2025-6/202561317933764860.png!400_400","daxiao":"18.2MB","create_time":"260305 "} 
 
 
