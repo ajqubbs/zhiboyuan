f3720733688c02e2e343e757483616cd57462cd8
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-03-12 20:46:45 +0800

    Update index.php via web editor

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 {"id":"6","title":"新凡人直播[腰疼]","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a840b8a1.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iD9Yj3jj4ekf","image1":"","image2":"","daxiao":"18.8MB","create_time":"260304 "}
 
-{"id":"6","title":"落花听雨[点播腰疼]","logo":"","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/ixYVY3kgbuab","image1":"","image2":"","daxiao":"44.8MB","create_time":"260312 "}
+{"id":"6","title":"落花听雨[点播腰疼]","logo":"https://free-img.400040.xyz/4/2026/03/12/69b2b5a81e6ce.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/ixYVY3kgbuab","image1":"","image2":"","daxiao":"44.8MB","create_time":"260312 "}
 
 {"id":"15","title":"至尊4K","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a225da58.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iOo3t3jrnp1e","image1":"","image2":"","daxiao":"58.8MB","create_time":"260304 "}
 
