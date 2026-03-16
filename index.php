f58efeeeb44fb0ce04f13ef9f294b9221f49f518
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-03-16 09:23:11 +0800

    Update index.php via web editor

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 {"id":"10","title":"极悦直播（解锁激活）","logo":"https://img.xiazainiao.com/up/2025/1230/6953f6f258e48_7768.jpg","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iFHGk3kpz45i","image1":"","image2":"","daxiao":"40.8MB","create_time":"260317"}
 
 
-{"id":"11","title":"青龙直播","logo":"https://img.xiazainiao.com/up/2025/1230/6953f6f258e48_7768.jpg","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iFHGk3kpz45i","image1":"","image2":"","daxiao":"40.8MB","create_time":"260317"}
+{"id":"11","title":"青龙TV直播","logo":"https://img.xiazainiao.com/up/2025/1230/6953f6f258e48_7768.jpg","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iFHGk3kpz45i","image1":"","image2":"","daxiao":"40.8MB","create_time":"260317"}
 
 {"id":"15","title":"至尊4K","logo":"https://free-img.400040.xyz/4/2026/03/04/69a84a225da58.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iOo3t3jrnp1e","image1":"","image2":"","daxiao":"58.8MB","create_time":"260304 "}
 
