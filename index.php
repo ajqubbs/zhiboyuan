0d9245a2b67dd8176414bfb6e4eebcb18d186cca
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-03-21 13:31:59 +0800

    Update index.php via web editor

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 {"id":"12","title":"GEEK-TV1.2.0［收藏点击5次密码8888］","logo":"https://free-img.400040.xyz/4/2026/03/20/69bd62b4888c8.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iyfeM3l22wlc","image1":"","image2":"","daxiao":"14.8MB","create_time":"260320"}
 
 
-{"id":"13","title":"酷影直播","logo":"https://free-img.400040.xyz/4/2026/03/20/69bd62b4888c8.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iwUgz3l4tl6f","image1":"","image2":"","daxiao":"24.8MB","create_time":"260321"}
+{"id":"13","title":"酷影直播","logo":"https://free-img.400040.xyz/4/2026/03/21/69be2d2a01acd.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iwUgz3l4tl6f","image1":"","image2":"","daxiao":"24.8MB","create_time":"260321"}
 
 
 
