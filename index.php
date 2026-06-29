a53e5ae2df87686b6477ee02b6be1b107fe93150
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-06-29 23:36:04 +0800

    Update index.php via web editor

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 {"id":"2","title":"问鼎助手电视端","logo":"https://i.imgs.ovh/2026/06/29/372d7c9804dec704e103e33aa7aec337.jpg","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iXCGf3tcexda","image1":"","image2":"","daxiao":"36MB","create_time":"260630 "}
 
-
 {"id":"2","title":"问鼎助手手机端","logo":"https://i.imgs.ovh/2026/06/29/372d7c9804dec704e103e33aa7aec337.jpg","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iSKXP3t0y32h","image1":"","image2":"","daxiao":"36MB","create_time":"260630 "}
 
 {"id":"1","title":"欢视助手","logo":"http://pic.9663.com/upload/2025-2/20252171348339087.png","desc1":"影视必备","desc":"","url":"https://wget.la/https://raw.githubusercontent.com/ajqubbs/androidtv/main/欢视助手_1.0.0.apk","image1":"","image2":"","daxiao":"7.6MB","create_time":"260609"}
