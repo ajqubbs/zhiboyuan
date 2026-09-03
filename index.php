f1e30c770ece14348ef9d58882cc0a5722f21ba0
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-09-03 20:18:28 +0800

    Update index.php via web editor

--- index.php
+++ index.php
@@ -27,7 +27,6 @@
 
 {"id":"7","title":"i香雨直播[腰疼]","logo":"https://free-img.400040.xyz/4/2026/03/23/69c08f6620f6d.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iZs5K3laj4dg","image1":"","image2":"","daxiao":"17.8MB","create_time":"260323"}
 
-
 {"id":"9","title":"凡🌱影","logo":"https://free-img.400040.xyz/4/2026/03/12/69b2b8d548fc4.png","desc1":"影视必备","desc":"","url":"https://lz.qaiu.top/parser?url=https://jxfd.lanzoub.com/iK0cW46ffbsf","image1":"","image2":"","daxiao":"37.8MB","create_time":"260312 "}
 
 {"id":"10","title":"抖音TV小書版","logo":"http://t7.baidu.com/it/u=4098962790,292963899&fm=74&app=80&f=JPEG?w=200&h=200&s=2B413B66170334FF4BFC489000005096","desc1":"影视必备","desc":"","url":"https://wget.la/https://raw.githubusercontent.com/ajqubbs/androidtv/main/抖音TV小書版_9.9.90.apk","image1":"","image2":"","daxiao":"16.8MB","create_time":"260401"}
